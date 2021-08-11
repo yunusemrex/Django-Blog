ebda3300a058f9860579ed97a4231d3518d0a375
yunusemrekarakus <yunusemreiletisim23@gmail.com>
2021-08-11 15:31:36 +0300

    Category model created

--- blog/blogapp/admin.py
+++ blog/blogapp/admin.py
@@ -1,3 +1,5 @@
+# from blog.blogapp.models.category import CategoryModel
 from django.contrib import admin
+from .models import CategoryModel
 
-# Register your models here.
+admin.site.register(CategoryModel)
